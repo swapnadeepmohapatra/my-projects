57e18be3f5e933480cfc9e6b6495c08ca99c03e2
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-05-05 21:40:23 +0530

    added banner

---
+++ src/Banner.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './main-bg.jpg';
+
+const Banner = () => {
+	return (
+		<div className="banner">
+			<img alt="" src={logo} objectFit="cover" objectPosition="50% 50%" />
+			<div className="container">
+				<div className="banner-details">
+					<h1>I'm Swapnadeep.</h1>
+					<ul className="sub-data">
+						<li>Developer</li>
+						<li>Student</li>
+						<li>Designer</li>
+					</ul>
+					<span>View all of my projects here.</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Banner;
